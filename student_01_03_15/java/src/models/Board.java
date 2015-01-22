b8abd2b655459d0e008f27bf29b516ffb06e470d
psoder3 <paulsoderquist3@gmail.com>
2015-01-21 20:42:25 -0700

    Added javadoc statements for Board.java

--- student_01_03_15/java/src/models/Board.java
+++ student_01_03_15/java/src/models/Board.java
@@ -3,22 +3,41 @@ package models;
 import classes.Hex;
 import classes.Port;
 
+/** Board class that contains a list of the hexes and a list of the harbors (ports)
+*
+* @author Team 2
+*/
 public class Board 
 {
-	
+	/** A list of all the hexes on the grid */
 	private Hex[] mHexes; //A list of all the hexes on the grid - it's only land tiles.
+	
+	/** A list of all the ports (harbors) on the grid */
 	private Port[] mPorts;
 	
+	/** Board Constructor
+	 * 
+	 * @return a new Board object
+	 */
 	public Board()
 	{
 		
 	}
 	
-	public Hex[] hexes()
+	/** Getter for hexes
+	 * 
+	 * @return a list of all the hexes on the grid
+	 */
+	public Hex[] getHexes()
 	{
 		return mHexes;
 	}
-	public Port[] ports()
+	
+	/** Getter for ports
+	 * 
+	 * @return a list of all the ports on the grid
+	 */
+	public Port[] getPorts()
 	{
 		return mPorts;
 	}
