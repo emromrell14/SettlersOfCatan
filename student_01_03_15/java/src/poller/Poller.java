d015df48ed4ae3fb7b8b640da8f6d420b7998ba0
Cannon Collins <cannoncollins@Cannons-MacBook-Pro-2.local>
2015-01-21 20:24:42 -0700

    made poller package and poller class file:

---
+++ student_01_03_15/java/src/poller/Poller.java
@@ -0,0 +1,42 @@
+package poller;
+
+public class Poller 
+{
+	public Poller() {
+		
+	}
+	
+	
+	
+	public void run() 
+	{
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	public boolean compareVersions() 
+	{
+		
+		
+		return true; // this is just a temporary default
+	}
+	
+	public void updateGUI() 
+	{
+		
+		
+	}
+	
+	
+	
+	public static void main(String[] args)
+	{
+		
+	}
+	
+}
