0947541d6ef0451df5ae9822378a42794879def2
psoder3 <paulsoderquist3@gmail.com>
2015-01-22 20:05:25 -0700

    Added Javadoc for GamesManager.java

--- student_01_03_15/java/src/facade/GamesManager.java
+++ student_01_03_15/java/src/facade/GamesManager.java
@@ -2,32 +2,61 @@ package facade;
 
 public class GamesManager 
 {
+	/**
+	 * Creates a GamesManager object
+	 * 
+	 * @return a new GamesManager object
+	 */
 	public GamesManager()
 	{
 		
 	}
 	
-	
+	/**
+	 * Gets the list of games in a JSON String
+	 * 
+	 * @return a String of JSON
+	 */
 	public String getGameList()
 	{
 		return null;
 	}
 
+	/**
+	 * Gets the create game info in a JSON String
+	 * 
+	 * @return a String of JSON
+	 */
 	public String createGame()
 	{
 		return null;
 	}
 
+	/**
+	 * Gets the join game info in a JSON String
+	 * 
+	 * @return a String of JSON
+	 */
 	public String joinGame()
 	{
 		return null;
 	}
 
+	/**
+	 * Gets the save game info in a JSON String
+	 * 
+	 * @return a String of JSON
+	 */
 	public String saveGame()
 	{
 		return null;
 	}
 
+	/**
+	 * Gets the load game info in a JSON String
+	 * 
+	 * @return a String of JSON
+	 */
 	public String loadGame() 
 	{
 		return null;
