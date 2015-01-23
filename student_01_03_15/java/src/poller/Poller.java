ae601af14dcbd7e2bba078f0511579fd4059053a
Cannon Collins <cannoncollins@Cannons-MBP-2.home>
2015-01-22 17:54:08 -0700

    Javadoc for poller

--- student_01_03_15/java/src/poller/Poller.java
+++ student_01_03_15/java/src/poller/Poller.java
@@ -1,13 +1,26 @@
 package poller;
 
+/** Poller class
+*
+* @author Team 2
+*/
+
 public class Poller 
 {
+    /**
+     * Creates the Poller object
+     *
+     * @return a new Poller object
+     */
 	public Poller() {
 		
 	}
 	
 	
-	
+	/**
+     * Run function to test the version and then update if needed
+     *
+     */
 	public void run() 
 	{
 		
@@ -18,14 +31,22 @@ public class Poller
 		
 	}
 	
-	
+	/**
+     * compareVersions to compare clients current version with possible updated game version
+     *
+     * @return boolean to tell if clients version is the same as the game or not
+     */
 	public boolean compareVersions() 
 	{
 		
 		
 		return true; // this is just a temporary default
 	}
-	
+    
+	/**
+     * updateGUI calls to update clients game to the game/s latest version
+     *
+     */
 	public void updateGUI() 
 	{
 		
@@ -33,7 +54,10 @@ public class Poller
 	}
 	
 	
-	
+	/**
+     * main function to be able to run this as a thread on a timer
+     *
+     */
 	public static void main(String[] args)
 	{
 		
