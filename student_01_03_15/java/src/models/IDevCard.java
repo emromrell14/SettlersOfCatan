54222c54c91176d0107ec2f36ca0a1bb1f20489d
emromrell14 <emromrell14@gmail.com>
2015-02-02 20:16:53 -0700

    Renamed getters and setters

--- student_01_03_15/java/src/models/IDevCard.java
+++ student_01_03_15/java/src/models/IDevCard.java
@@ -9,13 +9,13 @@ public interface IDevCard
 	 * 
 	 * @return true if card has been played, false otherwise.
 	 */
-	boolean getmPlayed();
+	boolean hasPlayed();
 	/** 
 	 * Gets development card type
 	 * 
 	 * @return one of the following development card type enum values: SOLDIER, YEAR_OF_PLENTY, MONOPOLY, ROAD_BUILD, MONUMENT
 	 */
-	DevCardType getmType();
+	DevCardType getType();
 	/** 
 	 * Initiates playing of this card
 	 * 
