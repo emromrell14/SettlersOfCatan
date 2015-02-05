b463ea3d668b3f9dd2bc3e5f38a42f3a4a0d85c9
seamane <eric_ctr_seaman247@yahoo.com>
2015-02-05 13:19:32 -0700

    update to Proxy JUnits

--- student_01_03_15/java/src/JUnitTests/ProxyTester.java
+++ student_01_03_15/java/src/JUnitTests/ProxyTester.java
@@ -2,71 +2,47 @@ package JUnitTests;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import proxy.IProxy;
 import proxy.Proxy;
 
 public class ProxyTester 
-{
-	private IProxy mProxy;
-	
-	@Before
-	public void initialize()
-	{
-		mProxy = new Proxy();
-	}
-	
+{	
 	@Test
 	public void testProxyPosts() 
 	{
+		IProxy mProxy = new Proxy();
 		System.out.println("PROXY POST TESTS:\n");
 		String response = null;
 		
-		System.out.print("Testing /user/login with valid input - ");
+		System.out.print("Testing /user/login - ");
 		response = mProxy.post("/user/login", "{\"username\": \"Sam\",\"password\": \"sam\"}");
 		assertTrue(!response.split(" ")[0].equalsIgnoreCase("Failed"));
 		System.out.println("PASSED");
-		System.out.print("Testing /user/login with invalid input - ");
-		response = mProxy.post("/user/login", "");
-		assertTrue(response.split(" ")[0].equalsIgnoreCase("Failed"));
-		System.out.println("PASSED");
 		
-		System.out.print("Testing /user/register with valid input - ");
+		System.out.print("Testing /user/register - ");
 		response = mProxy.post("/user/register", "{\"username\": \""+ Math.random() +"\",\"password\": \"string\"}");
 		assertTrue(!response.split(" ")[0].equalsIgnoreCase("Failed"));
 		System.out.println("PASSED");
-		System.out.print("Testing /user/register with invalid input - ");
-		response = mProxy.post("/user/register", "");
-		assertTrue(response.split(" ")[0].equalsIgnoreCase("Failed"));
-		System.out.println("PASSED");
 		
-		System.out.print("Testing /games/create with valid input - ");
-		response = mProxy.post("/games/create", "{\"randomTiles\": \"false\",\"randomNumbers\": \"false\",\"randomPorts\": \"false\",\"name\": \"string\"}");
+		System.out.print("Testing /games/create - ");
+		response = mProxy.post("/games/create", "{\"randomTiles\": \"false\",\"randomNumbers\": \"false\",\"randomPorts\": \"false\",\"name\": \"test2\"}");
 		assertTrue(!response.split(" ")[0].equalsIgnoreCase("Failed"));
 		System.out.println("PASSED");
-		System.out.print("Testing /games/create with invalid input - ");
-		response = mProxy.post("/games/create", "");
-		assertTrue(response.split(" ")[0].equalsIgnoreCase("Failed"));
-		System.out.println("PASSED");
 		
-		System.out.print("Testing /games/join with valid input - ");
-		response = mProxy.post("/games/join", "{\"id\": 4,\"color\": \"red\"}");
+		System.out.print("Testing /games/join - ");
+		response = mProxy.post("/games/join", "{\"id\": 4,\"color\": \"blue\"}");
 		assertTrue(!response.split(" ")[0].equalsIgnoreCase("Failed"));
-		System.out.println("PASSED");
-		System.out.print("Testing /games/join with invalid input - ");
-		response = mProxy.post("/games/join", "");
-		assertTrue(response.split(" ")[0].equalsIgnoreCase("Failed"));
 		System.out.println("PASSED");
 
 		System.out.print("Testing /games/save - ");
-		response = mProxy.post("/games/save", "");
+		response = mProxy.post("/games/save", "{\"id\": 1,\"name\": \"AIGame\"}");
 		assertTrue(response.split(" ")[0].equalsIgnoreCase("Failed"));
 		System.out.println("PASSED");
 		
 		System.out.print("Testing /games/load - ");
-		response = mProxy.post("/games/load", "");
+		response = mProxy.post("/games/load", "{\"name\": \"AIGame\"}");
 		assertTrue(response.split(" ")[0].equalsIgnoreCase("Failed"));
 		System.out.println("PASSED");
 		
@@ -80,22 +56,14 @@ public class ProxyTester
 		assertTrue(response.split(" ")[0].equalsIgnoreCase("Failed"));
 		System.out.println("PASSED");
 		
-		System.out.print("Testing /game/addAI with valid input - ");
+		System.out.print("Testing /game/addAI - ");
 		response = mProxy.post("/game/addAI", "{\"AIType\": \"LARGEST_ARMY\"}");
 		assertTrue(!response.split(" ")[0].equalsIgnoreCase("Failed"));
 		System.out.println("PASSED");
-		System.out.print("Testing /game/addAI with valid input - ");
-		response = mProxy.post("/game/addAI", "");
-		assertTrue(response.split(" ")[0].equalsIgnoreCase("Failed"));
-		System.out.println("PASSED");
 		
-		System.out.print("Testing /moves/sendChat with valid input - ");
+		System.out.print("Testing /moves/sendChat - ");
 		response = mProxy.post("/moves/sendChat", "{\"type\": \"sendChat\",\"playerIndex\": 1,\"content\": \"string\"}");
 		assertTrue(!response.split(" ")[0].equalsIgnoreCase("Failed"));
-		System.out.println("PASSED");
-		System.out.print("Testing /moves/sendChat with invalid input - ");
-		response = mProxy.post("/moves/sendChat", "");
-		assertTrue(response.split(" ")[0].equalsIgnoreCase("Failed"));
 		System.out.println("PASSED");
 		
 		System.out.print("Testing /moves/rollNumber - ");
@@ -178,21 +146,33 @@ public class ProxyTester
 		assertTrue(response.split(" ")[0].equalsIgnoreCase("Failed"));
 		System.out.println("PASSED");
 		
-		System.out.print("Testing /util/changeLogLevel with valid input - ");
+		System.out.print("Testing /util/changeLogLevel - ");
 		response = mProxy.post("/util/changeLogLevel", "{\"logLevel\": \"ALL\"}");
 		assertTrue(!response.split(" ")[0].equalsIgnoreCase("Failed"));
-		System.out.println("PASSED");
-		System.out.print("Testing /util/changeLogLevel with invalid input - ");
-		response = mProxy.post("/util/changeLogLevel", "");
-		assertTrue(response.split(" ")[0].equalsIgnoreCase("Failed"));
 		System.out.println("PASSED");
 	}
 
 	@Test
 	public void testProxyGets()
 	{
+		IProxy mProxy = new Proxy();
 		System.out.println("PROXY GET TESTS:\n");
 		String response = null;
+
+		System.out.print("Testing /user/login - ");
+		response = mProxy.post("/user/login", "{\"username\": \"Sam\",\"password\": \"sam\"}");
+		assertTrue(!response.split(" ")[0].equalsIgnoreCase("Failed"));
+		System.out.println("PASSED");
+		
+		System.out.print("Testing /games/create - ");
+		response = mProxy.post("/games/create", "{\"randomTiles\": \"false\",\"randomNumbers\": \"false\",\"randomPorts\": \"false\",\"name\": \"test\"}");
+		assertTrue(!response.split(" ")[0].equalsIgnoreCase("Failed"));
+		System.out.println("PASSED");
+		
+		System.out.print("Testing /games/join - ");
+		response = mProxy.post("/games/join", "{\"id\": 3,\"color\": \"red\"}");
+		assertTrue(!response.split(" ")[0].equalsIgnoreCase("Failed"));
+		System.out.println("PASSED");
 		
 		System.out.print("Testing /games/list - ");
 		response = mProxy.get("/games/list");
@@ -212,6 +192,6 @@ public class ProxyTester
 		System.out.print("Testing /game/listAI - ");
 		response = mProxy.get("/game/listAI");
 		assertTrue(!response.split(" ")[0].equalsIgnoreCase("Failed"));
-		System.out.println("PASSED");
+		System.out.println("PASSED\n");
 	}
 }
