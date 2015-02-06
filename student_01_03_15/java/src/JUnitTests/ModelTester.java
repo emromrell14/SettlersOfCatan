38b4b0aa5748a567cac0ac6243a464738e08cae2
psoder3 <paulsoderquist3@gmail.com>
2015-02-06 02:14:55 -0700

    added stubbed out ModelTester class for JUnits

---
+++ student_01_03_15/java/src/JUnitTests/ModelTester.java
@@ -0,0 +1,145 @@
+package JUnitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ModelTester 
+{
+
+	@Before
+	public void initialize() 
+	{		
+	}
+	
+	@Test
+	public void testCanAffordRoad()
+	{
+		
+	}
+	
+	@Test
+	public void testCanPlaceRoad()
+	{
+		
+	}
+	
+	@Test
+	public void testCanAffordSettlement()
+	{
+		
+	}
+	
+	@Test
+	public void testCanPlaceSettlement()
+	{
+		
+	}
+	
+	@Test
+	public void testCanAffordCity()
+	{
+		
+	}
+	
+	@Test
+	public void testCanBuyDevCard()
+	{
+		
+	}
+	
+	/* NOTE TO ERIC R.
+	 * MOST OF THE REMAINING TESTS HERE ARE REALLY ONLY USING THE MODEL
+	 * IN ORDER TO CHECK THE TURN TRACKER AND VERIFY THAT IT IS THIS 
+	 * PLAYER'S TURN
+	 * 
+	 * THEN THEY CHECK THE OTHER CONDITIONS USING THE CORRESPONDING METHODS
+	 * IN THE PLAYER CLASS
+	 */
+	@Test
+	public void testCanPlayDevCard()
+	{
+		
+	}
+	
+	@Test
+	public void testCanOfferTrade()
+	{
+		
+	}
+	
+	@Test
+	public void testCanAcceptTrade()
+	{
+		
+	}
+	
+	@Test
+	public void testCanMaritimeTrade()
+	{
+		
+	}
+	
+	@Test
+	public void testCanRollDice()
+	{
+		
+	}
+	
+	@Test
+	public void testCanDiscard()
+	{
+		
+	}
+	
+	@Test
+	public void testCanFinishTurn()
+	{
+		
+	}
+	
+	@Test
+	public void testCanPlayYearOfPlenty()
+	{
+		
+	}
+	
+	@Test
+	public void testCanPlaySoldier()
+	{
+		
+	}
+	
+	@Test
+	public void testCanPlayMonopoly()
+	{
+		
+	}
+	
+	@Test
+	public void testCanPlayMonument()
+	{
+		
+	}
+	
+	@Test
+	public void testCanPlaceRobber()
+	{
+		
+	}
+	
+	@Test
+	public void testCanPlayRoadBuilder()
+	{
+		
+	}
+	
+	
+	@After
+	public void tearDown()
+	{
+		return;
+	}
+}
