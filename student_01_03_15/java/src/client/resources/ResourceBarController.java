5724ecdaf170d2ee0e35cb813962ad72354db92d
Cannon Collins <cannoncollins@Cannons-MBP.app.byu.edu>
2015-02-18 19:12:52 -0700

    Made observer, few function changes, not finished

--- student_01_03_15/java/src/client/resources/ResourceBarController.java
+++ student_01_03_15/java/src/client/resources/ResourceBarController.java
@@ -3,24 +3,34 @@ package client.resources;
 import java.util.*;
 
 import client.base.*;
+import client.map.IMapView;
+import client.map.MapController;
+import facade.MasterManager;
 
 
 /**
  * Implementation for the resource bar controller
  */
-public class ResourceBarController extends Controller implements IResourceBarController {
+public class ResourceBarController extends Controller implements IResourceBarController, Observer 
+{
 
 	private Map<ResourceBarElement, IAction> elementActions;
+	private MasterManager master;
+//	private IMapView mapView;
 	
-	public ResourceBarController(IResourceBarView view) {
+	public ResourceBarController(IResourceBarView view) 
+	{
 
 		super(view);
 		
 		elementActions = new HashMap<ResourceBarElement, IAction>();
+		master = MasterManager.getInstance();
+		master.getModelManager().addObserver(this);
 	}
 
 	@Override
-	public IResourceBarView getView() {
+	public IResourceBarView getView() 
+	{
 		return (IResourceBarView)super.getView();
 	}
 
@@ -30,43 +40,82 @@ public class ResourceBarController extends Controller implements IResourceBarCon
 	 * @param element The resource bar element with which the action is associated
 	 * @param action The action to be executed
 	 */
-	public void setElementAction(ResourceBarElement element, IAction action) {
+	public void setElementAction(ResourceBarElement element, IAction action) 
+	{
 
 		elementActions.put(element, action);
 	}
 
 	@Override
-	public void buildRoad() {
+	public void buildRoad() 
+	{
 		executeElementAction(ResourceBarElement.ROAD);
+		if (master.canAffordCity(master.getPlayerIndex()))
+		{
+			
+		}
+		
 	}
 
 	@Override
-	public void buildSettlement() {
+	public void buildSettlement() 
+	{
 		executeElementAction(ResourceBarElement.SETTLEMENT);
+		if (master.canAffordSettlement(master.getPlayerIndex()))
+		{
+			
+		}
+		
 	}
 
 	@Override
-	public void buildCity() {
+	public void buildCity() 
+	{
 		executeElementAction(ResourceBarElement.CITY);
+		if (master.canAffordCity(master.getPlayerIndex())) 
+		{
+			
+		}
+		
 	}
 
 	@Override
-	public void buyCard() {
+	public void buyCard() 
+	{
 		executeElementAction(ResourceBarElement.BUY_CARD);
+		if (master.canBuyDevCard(master.getPlayerIndex()))
+		{
+			
+		}
+		
 	}
 
 	@Override
-	public void playCard() {
+	public void playCard() 
+	{
 		executeElementAction(ResourceBarElement.PLAY_CARD);
+		
+		
 	}
 	
-	private void executeElementAction(ResourceBarElement element) {
+	private void executeElementAction(ResourceBarElement element) 
+	{
 		
-		if (elementActions.containsKey(element)) {
+		if (elementActions.containsKey(element)) 
+		{
 			
 			IAction action = elementActions.get(element);
 			action.execute();
 		}
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
 	}
 
 }
