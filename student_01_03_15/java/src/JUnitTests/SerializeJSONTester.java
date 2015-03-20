621743f5af4935e7d198092b646868c6f2740f30
Paul Soderquist <paulsoderquist3@gmail.com>
2015-03-20 11:53:30 -0600

    Added another big client test

--- student_01_03_15/java/src/JUnitTests/SerializeJSONTester.java
+++ student_01_03_15/java/src/JUnitTests/SerializeJSONTester.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import models.*;
 
@@ -12,10 +11,8 @@ import org.junit.Test;
 
 import server.User;
 import shared.definitions.CatanColor;
-import shared.definitions.DevCardType;
 import shared.definitions.HexType;
 import shared.definitions.PortType;
-import shared.definitions.ResourceType;
 import shared.locations.EdgeDirection;
 import shared.locations.EdgeLocation;
 import shared.locations.HexLocation;
@@ -26,22 +23,13 @@ import JSONmodels.*;
 public class SerializeJSONTester {
 
 	@Test
-	public void test() {
-		//ClientModel
+	public void test1() {
+		//ClientModel Test 1
 		
 		try
 		{
 			IGame game = new Game();
-			
-			ResourceList rl = new ResourceList();
-			rl.addBrick(4);
-			rl.addOre(2);
-			rl.addSheep(5);
-			rl.addWheat(0);
-			rl.addWood(1);
-			
-			
-			
+					
 			ResourceList bank = new ResourceList();
 			bank.addBrick(20);
 			bank.addOre(15);
@@ -57,12 +45,12 @@ public class SerializeJSONTester {
 			board.addHex(new Hex(new HexLocation(3,-2), HexType.ORE, new TokenValue(12)));
 			board.addHex(new Hex(new HexLocation(-1,2), HexType.SHEEP, new TokenValue(7)));
 			
-			List<Message> messageList = new ArrayList();
+			List<Message> messageList = new ArrayList<Message>();
 			messageList.add(new Message("You can shut up!", "Eric"));
 			messageList.add(new Message("Buckwheat!", "Eric"));
 			messageList.add(new Message("Ok", "Paul"));
 			
-			List<Message> logList = new ArrayList();
+			List<Message> logList = new ArrayList<Message>();
 			logList.add(new Message("Paul built a road", "Paul"));
 			logList.add(new Message("Eric won the game", "Eric"));
 			
@@ -103,15 +91,20 @@ public class SerializeJSONTester {
 			int numRoads = 8;
 			boolean playedDevCard = true;
 			
+			ResourceList playerResources = new ResourceList();
+			playerResources.addBrick(4);
+			playerResources.addOre(2);
+			playerResources.addSheep(5);
+			playerResources.addWheat(0);
+			playerResources.addWood(1);
+			
 			Player p = new Player();
 			
-			//CatanColor.GREEN, "Paul", index, id, rl, numRoads, numSettlements, numCities, soldiers, victoryPoints, 
-			//false, playedDevCard, monuments, new User(id), null, null, null, playerDevCards
 			p.setColor(CatanColor.GREEN);
 			p.setName("Paul");
 			p.setPlayerIndex(index);	
 			p.setPlayerID(id);
-			p.setResources(rl);
+			p.setResources(playerResources);
 			p.setRoadCount(numRoads);
 			p.setSettlementCount(numSettlements);
 			p.setCityCount(numCities);
@@ -170,6 +163,8 @@ public class SerializeJSONTester {
 		}
 		catch (Exception e)
 		{
+			System.out.println(e);
+			System.out.println("there may have been a bad index in test 1");
 			// Bad Index
 		}
 		
@@ -277,5 +272,158 @@ public class SerializeJSONTester {
 		assertTrue(vertexObject.toJSON().equals(JSONVertexObject));
 		*/
 	}
+	
+	@Test
+	public void test2() {
+		//ClientModel Test 2
+		
+		try
+		{
+			IGame game = new Game();
+			
+			ResourceList bank = new ResourceList();
+			bank.addBrick(353);
+			bank.addOre(10);
+			bank.addSheep(22);
+			bank.addWheat(33);
+			bank.addWood(72);
+			
+			Board board = new Board();
+			board.addCity(new Building(new Index(1), new VertexLocation(new HexLocation(3,-1), VertexDirection.NorthWest)));
+			board.addCity(new Building(new Index(2), new VertexLocation(new HexLocation(-2,1), VertexDirection.SouthEast)));
+			board.addSettlement(new Building(new Index(0), new VertexLocation(new HexLocation(1,2), VertexDirection.SouthWest)));
+			board.addSettlement(new Building(new Index(0), new VertexLocation(new HexLocation(1,-3), VertexDirection.East)));
+			board.addRoad(new Road(new Index(0), new EdgeLocation(new HexLocation(0,0), EdgeDirection.South)));
+			board.addPort(new Port(PortType.WHEAT, new HexLocation(-2,1), EdgeDirection.South));
+			board.addHex(new Hex(new HexLocation(3,-2), HexType.DESERT, new TokenValue(7)));
+			board.addHex(new Hex(new HexLocation(-1,2), HexType.BRICK, new TokenValue(1)));
+			
+			List<Message> messageList = new ArrayList<Message>();
+			messageList.add(new Message("", "Eric"));
+			messageList.add(new Message("A", "Cannon"));
+			messageList.add(new Message("B", "Michael"));
+			
+			List<Message> logList = new ArrayList<Message>();
+			logList.add(new Message("Michael bought a dev card", "Michael"));
+			logList.add(new Message("Eric won the game", "Eric"));
+			
+			TurnTracker turnTracker = new TurnTracker();
+			turnTracker.setCurrentTurn(new Index(3));
+			turnTracker.setLargestArmy(new Index(2));
+			turnTracker.setLongestRoad(new Index(1));
+			turnTracker.setStatus(Status.DISCARDING);
+			
+			List<DevCard> devCards = new ArrayList<DevCard>();
+			devCards.add(new YearOfPlenty());
+			devCards.add(new RoadBuild());
+			
+			ResourceList offer = new ResourceList(-1,25,21,20,43);
+			
+			Trade tradeObject = new Trade(new Index(2), new Index(3), offer);
+			
+			List<DevCard> playerDevCards = new ArrayList<DevCard>();
+			Monopoly monopoly = new Monopoly();
+			monopoly.setNew(false);
+			Soldier soldier = new Soldier();
+			soldier.setNew(true);
+
+			
+			playerDevCards.add(monopoly);
+			playerDevCards.add(soldier);
+			
+			Index index = new Index(1);
+			int id = 4444;
+			
+			int soldiers = 55;
+			int victoryPoints = 11;
+			int monuments = 0;
+			int numSettlements = 2;
+			int numCities = 4;
+			int numRoads = 1;
+			boolean playedDevCard = false;
+			
+			ResourceList playerResources = new ResourceList();
+			playerResources.addBrick(2);
+			playerResources.addOre(3);
+			playerResources.addSheep(4);
+			playerResources.addWheat(5);
+			playerResources.addWood(6);
+			
+			Player p = new Player();
+			
+			p.setColor(CatanColor.PUCE);
+			p.setName("Eric");
+			p.setPlayerIndex(index);	
+			p.setPlayerID(id);
+			p.setResources(playerResources);
+			p.setRoadCount(numRoads);
+			p.setSettlementCount(numSettlements);
+			p.setCityCount(numCities);
+			p.setSoldierCount(soldiers);
+			p.setVictoryPointCount(victoryPoints);
+			p.setHasPlayedDevCard(playedDevCard);
+			p.setHasDiscarded(true);
+			p.setMonuments(monuments);
+			p.setUser(new User(id));
+			p.setDevCards(playerDevCards);
+			
+			//game.addPlayer(p);
+			game.setBank(bank);
+			game.setBoard(board);
+			game.setId(63);
+			game.setChat(messageList);
+			game.setLog(logList);
+			game.setName("testing game 2");
+			game.setRobber(new Robber(new HexLocation(3,3)));
+			game.setVersion(2000);
+			game.setWinner(1);
+			game.setTurnTracker(turnTracker);
+			game.setDevCards(devCards);
+			game.setTrade(tradeObject);
+			
+			
+			ClientModelJSON model = new ClientModelJSON(game);
+			String actualJson = model.toJSON();
+			
+			String expectedJson = "{\"bank\":{\"brick\":353,\"ore\":10,\"sheep\":22,\"wheat\":33,\"wood\":72},"+
+			"\"chat\":{\"lines\":[{\"message\":\"\",\"source\":\"Eric\"},{\"message\":\"A\",\"source\":\"Cannon\"},{\"message\":\"B\",\"source\":\"Michael\"}]},"
+					+ "\"log\":{\"lines\":[{\"message\":\"Michael bought a dev card\",\"source\":\"Michael\"},{\"message\":\"Eric won the game\",\"source\":\"Eric\"}]},"
+					+ "\"map\":{\"hexes\":[{\"location\":{\"x\":3,\"y\":-2},\"resource\":\"desert\",\"number\":7},{\"location\":{\"x\":-1,\"y\":2},\"resource\":\"brick\",\"number\":1}],"
+					+ "\"ports\":[{\"resource\":\"wheat\",\"location\":{\"x\":-2,\"y\":1},\"direction\":\"S\",\"ratio\":2}],"
+					+ "\"roads\":[{\"owner\":0,\"location\":{\"x\":0,\"y\":0,\"direction\":\"S\"}}],"
+					+ "\"settlements\":[{\"owner\":0,\"location\":{\"x\":1,\"y\":2,\"direction\":\"SW\"}},{\"owner\":0,\"location\":{\"x\":1,\"y\":-3,\"direction\":\"E\"}}],"
+					+ "\"cities\":[{\"owner\":1,\"location\":{\"x\":3,\"y\":-1,\"direction\":\"NW\"}},{\"owner\":2,\"location\":{\"x\":-2,\"y\":1,\"direction\":\"SE\"}}],"
+					+ "\"radius\":3,"
+					+ "\"robber\":{\"x\":3,\"y\":3}},"
+					+ "\"players\":"
+					+ 	"["
+					/*+ 		
+					  		"{\"color\":\"puce\",\"discarded\":true,\"monuments\":0,\"name\":\"Eric\","
+					+ 		"\"newDevCards\":{\"monopoly\":0,\"monument\":0,\"roadBuilding\":0,\"soldier\":1,\"yearOfPlenty\":0},"
+					+ 		"\"oldDevCards\":{\"monopoly\":1,\"monument\":0,\"roadBuilding\":0,\"soldier\":0,\"yearOfPlenty\":0},"
+					+ 		"\"playerIndex\":1,\"playedDevCard\":false,\"playerID\":4444,"
+					+ 		"\"resources\":{\"brick\":2,\"ore\":3,\"sheep\":4,\"wheat\":5,\"wood\":6},"
+					+ 		"\"roads\":1,\"settlements\":2,\"cities\":4,"
+					+ 		"\"soldiers\":55,\"victoryPoints\":11}"
+					*/
+					+ 	"],"
+					+ "\"tradeOffer\":{\"sender\":2,\"receiver\":3,\"offer\":{\"brick\":-1,\"ore\":25,\"sheep\":21,\"wheat\":20,\"wood\":43}},"
+					+ "\"turnTracker\":{\"currentTurn\":3,\"status\":\"DISCARDING\",\"longestRoad\":1,\"largestArmy\":2},\"version\":2000,\"winner\":1}";
+			System.out.println(expectedJson);
+			System.out.println(actualJson);
+			
+			
+			assertTrue(actualJson.equals(expectedJson));
+			
+
+		}
+		catch (Exception e)
+		{
+			System.out.println(e);
+			System.out.println("there may have been a bad index in test 2");
+			// Bad Index
+		}
+	}
+	
 
 }
